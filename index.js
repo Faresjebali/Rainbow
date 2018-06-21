8d1547de21fc592c01342d7bd41c3f517fdea663
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-21 02:09:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
