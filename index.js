c89a6df736ba35cee2971330d60fe6060a1508ff
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-21 02:03:49 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  client.user.setActivity('BlazePVP');
+});
+
+client.login('NDU5MTQ1OTM5NDA0NDU1OTM2.Dgx81A.9mbw-PW90ftxjEhBgxZgh4c1BTE');
