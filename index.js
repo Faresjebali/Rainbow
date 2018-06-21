bd92250eb2c1737c359b5c30603d79657169d2a6
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-21 02:16:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.get('456895943175045142').roles.find('name', 'RainBow').edit({color: 'RANDOM'}) 
-    },10000);
-});
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   client.user.setActivity('BlazePVP');
 });
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('456895943175045142').roles.find('name', 'Rainbow').edit({color: 'RANDOM'})
+    },10000);
+});
+
 
 client.login('NDU5MTQ1OTM5NDA0NDU1OTM2.Dgx81A.9mbw-PW90ftxjEhBgxZgh4c1BTE');
